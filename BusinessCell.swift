c8bd6d4830b0766ba4105cc47c6695dacc19d553
Kevaughn <kcc165@msstate.edu>
2016-02-13 17:35:37 -0800

    basic

---
+++ BusinessCell.swift
@@ -0,0 +1,63 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by kevaughn charles on 2/13/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+    
+    @IBOutlet weak var thumbImageView: UIImageView!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    var business: Business!{
+        didSet{
+            nameLabel.text = business.name
+            
+            categoriesLabel.text = business.categories
+            distanceLabel.text = business.distance
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            addressLabel.text = business.address
+            thumbImageView.setImageWithURL(business.imageURL!)
+            ratingImageView.setImageWithURL(business.ratingImageURL!)
+            
+           
+            
+            
+            
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+        
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
